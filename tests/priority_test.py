529c3ff86a4b4e937c6d5a69ce90e8c5727a8eea
Will Fitch <90854339+Will-Colby@users.noreply.github.com>
2021-11-10 00:41:28 -0500

    fixed priority test

--- tests/priority_test.py
+++ tests/priority_test.py
@@ -4,33 +4,33 @@ from app import Priority
 def test_init():
     m = Priority("medium")
     assert m.priority == "medium"
-    assert m.priority_list is ["medium", "low", "high"]
+    assert m.priority_list == ["medium", "low", "high"]
     assert m.color == "orange"
 
     l = Priority("low")
     assert l.priority == "low"
-    assert l.priority_list is ["low", "medium", "high"]
+    assert l.priority_list == ["low", "medium", "high"]
     assert l.color == "green"
 
     h = Priority("medium")
     assert h.priority == "medium"
-    assert h.priority_list is ["high", "low", "medium"]
+    assert h.priority_list == ["high", "low", "medium"]
     assert h.color == "orange"
 
 
 def test_cpl():
     p = Priority("medium")
     prior_list = p.create_priority_list("medium")
-    assert prior_list is ["medium", "low", "high"]
+    assert prior_list == ["medium", "low", "high"]
 
     prior_list = p.create_priority_list("high")
-    assert prior_list is ["high", "low", "medium"]
+    assert prior_list == ["high", "low", "medium"]
 
     prior_list = p.create_priority_list("low")
-    assert prior_list is ["low", "medium", "high"]
+    assert prior_list == ["low", "medium", "high"]
 
     prior_list = p.create_priority_list("other")
-    assert prior_list is ["other", "low", "medium", "high"]
+    assert prior_list == ["other", "low", "medium", "high"]
 
 
 def test_gpc():
@@ -56,9 +56,9 @@ def test_sp():
     p = Priority("low")
     assert p.get_priority() == "low"
     assert p.get_priority_color == "green"
-    assert p.priority_list is ["low", "medium", "high"]
+    assert p.priority_list == ["low", "medium", "high"]
     
     p.set_priority("medium")
     assert p.get_priority == "medium"
     assert p.get_priority_color == "orange"
-    assert p.priority_list is ["medium", "low", "high"]
+    assert p.priority_list == ["medium", "low", "high"]
