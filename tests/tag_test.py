df9a1578c773fce48ac17572a21f5ff07a1a69e0
Will-Colby <90854339+Will-Colby@users.noreply.github.com>
2021-11-10 01:45:54 -0500

    added lots of tests

---
+++ tests/tag_test.py
@@ -0,0 +1,45 @@
+import app as a
+
+def test_tag():
+    tags = []
+    tag1 = a.Tag(tags)
+    assert len(tag1.tag_list) == 1
+    tags.append("cat")
+    tag1.set_tag("cat",tags)
+    assert tag1.get_tag() == "cat"
+    assert tag1.tag_list[0] == "cat"
+    
+def test_get_tag():
+    tags = ["dog"]
+    tag1 = a.Tag(tags)
+    assert tag1.get_tag() == tag1.tag
+    assert tag1.tag == "none"
+    
+def test_get_with_set():
+    tags = ["mouse"]
+    tag1 = a.Tag(tags)
+    assert tag1.get_tag() == "none"
+    tag1.set_tag("cat",tags)
+    assert tag1.get_tag() == "cat"
+
+def test_set_with_update():
+    tags = ["cat","mouse"]
+    tag1 = a.Tag(tags)
+    assert tag1.get_tag() == "none"
+    tag1.set_tag("cat",tags)
+    assert tag1.get_tag() == "cat"
+    assert tag1.tag_list == ["cat","mouse"]
+    tag1.set_tag("mouse",tags)
+    assert tag1.get_tag() == "mouse"
+    assert tag1.tag_list == ["mouse","cat"]
+    
+def test_update():
+    tags = ["cat","mouse"]
+    tag1 = a.Tag(tags)
+    assert tag1.get_tag() == "none"
+    tag1.set_tag("cat",tags)
+    tag1.update_tag_list(["cat","dog"])
+    assert tag1.tag_list == ["cat","dog"]
+    tag1.update_tag_list(["dog","cat"])
+    assert tag1.tag_list == ["cat","dog"]
+    
