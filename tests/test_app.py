deb997b9c90e14d27fb2fb3018880b127ef6d1af
Will-Colby <90854339+Will-Colby@users.noreply.github.com>
2021-11-10 01:33:11 -0500

    added lots of tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -11,10 +11,71 @@ def test_tag():
     assert tag1.get_tag() == "cat"
     assert tag1.tag_list[0] == "cat"
     
+# while we cannot directly test the flask html methods
+# we can ensure that they are appropriately intertwined
+# with the flask context by verifying they fail outside
+# of it.
+    
+def test_index():
+    failsOutOfContext = False
+    try:
+        a.index()
+    except:
+        failsOutOfContext = True
+    assert failsOutOfContext
+    
 def test_remove():
     failsOutOfContext = False
     try:
         a.remove(0)
+    except:
+        failsOutOfContext = True
+    assert failsOutOfContext
+    
+def test_add():
+    failsOutOfContext = False
+    try:
+        a.add()
+    except:
+        failsOutOfContext = True
+    assert failsOutOfContext
+    
+def test_prioritize():
+    failsOutOfContext = False
+    try:
+        a.prioritize(0)
+    except:
+        failsOutOfContext = True
+    assert failsOutOfContext
+    
+def test_check():
+    failsOutOfContext = False
+    try:
+        a.check(0)
+    except:
+        failsOutOfContext = True
+    assert failsOutOfContext
+    
+def test_addTag():
+    failsOutOfContext = False
+    try:
+        a.addTag()
+    except:
+        failsOutOfContext = True
+    assert failsOutOfContext
+    
+def test_removeTag():
+    failsOutOfContext = False
+    try:
+        a.removeTag("some_tag")
+    except:
+        failsOutOfContext = True
+    assert failsOutOfContext
+    
+def test_tagItem():
+    failsOutOfContext = False
+    try:
+        a.tagItem(0)
     except:
         failsOutOfContext = True
     assert failsOutOfContext
