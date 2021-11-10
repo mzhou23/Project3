32794a42637e051546b45a65b8ae22e7698e1b6c
Will-Colby <90854339+Will-Colby@users.noreply.github.com>
2021-11-10 01:36:05 -0500

    removed Tag test

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,15 +1,6 @@
 from flask import Flask
 
 import app as a
-
-def test_tag():
-    tags = []
-    tag1 = a.Tag(tags)
-    assert len(tag1.tag_list) == 1
-    tags.append("cat")
-    tag1.set_tag("cat",tags)
-    assert tag1.get_tag() == "cat"
-    assert tag1.tag_list[0] == "cat"
     
 # while we cannot directly test the flask html methods
 # we can ensure that they are appropriately intertwined
